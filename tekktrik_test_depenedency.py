c59ede74a2108592f1b9715f18b6d21403a55345
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-06-22 16:37:22 -0400

    Update tekktrik_test_depenedency.py

--- tekktrik_test_depenedency.py
+++ tekktrik_test_depenedency.py
@@ -2,18 +2,3 @@ from typing import Any
 
 def printaroo(thing: Any) -> None:
     print("thing:", thing)
-    print("type:", type(thing))
-    
-def printyprinty(thing: Any) -> None:
-    for _ in range(2):
-        print(thing)
-        
-def noprint(thing: Any) -> None:
-    pass
-
-def printonlytype(thing: Any) -> None:
-    print("In printonlytype...")
-    print("type:", type(thing))
-    
-def printstandard() -> None:
-    print("standard")
