e9c5459d862918d7f5eb4b10b649587cd46710bd
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-06-22 15:06:03 -0400

    Update tekktrik_test_depenedency.py

--- tekktrik_test_depenedency.py
+++ tekktrik_test_depenedency.py
@@ -3,3 +3,7 @@ from typing import Any
 def printaroo(thing: Any) -> None:
     print("thing:", thing)
     print("type:", type(thing))
+    
+def printyprinty(thing: Any) -> None:
+    for _ in range(2):
+        print(thing)
