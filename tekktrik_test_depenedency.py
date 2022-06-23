acbe6bfc0bfc0afa2763215d93932b981eb4b5cf
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-06-22 20:20:08 -0400

    Update tekktrik_test_depenedency.py

--- tekktrik_test_depenedency.py
+++ tekktrik_test_depenedency.py
@@ -1,9 +1,7 @@
 from typing import Any
 
 def printaroo(thing: Any) -> None:
-    print("Starting now...")
     print("First...")
     print("thing type:", type(thing))
     print("and then...")
     print("!")
-    print("Ending now...")
