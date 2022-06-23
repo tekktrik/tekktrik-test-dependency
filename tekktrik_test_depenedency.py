e4e433aca462c407c4928566ef620979321bb40d
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-06-22 20:26:06 -0400

    Update tekktrik_test_depenedency.py

--- tekktrik_test_depenedency.py
+++ tekktrik_test_depenedency.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-def printaroo(thing: Any) -> None:
+def printaroonie(thing: Any) -> None:
     print("First...")
     print("thing type:", type(thing))
     print("and then...")
